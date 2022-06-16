69ce111dc0e08eb120017313c2e4a2065bf86d25
Nikhil Mohadikar <mohadikar59@gmail.com>
2022-06-16 12:16:27 +0530

    Adds User and Tweet model.

---
+++ db/migrate/20220616055044_add_users.rb
@@ -0,0 +1,14 @@
+class AddUsers < ActiveRecord::Migration[7.0]
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
